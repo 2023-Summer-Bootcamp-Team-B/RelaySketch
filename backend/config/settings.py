d892b530085b8c3dd8a6a3a78b2a4347d9b9b8dc
Mayreeel <kuwon15@gmail.com>
2023-07-07 15:04:37 +0900

    chore : pymysql설치

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
