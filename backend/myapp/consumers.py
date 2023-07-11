045759228499907dae75e55c156ec7ddb860461a
Mayreeel <kuwon15@gmail.com>
2023-07-11 20:57:54 +0900

    fix : routing 수정

--- backend/myapp/consumers.py
+++ backend/myapp/consumers.py
@@ -1,15 +1,24 @@
+import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class SomeConsumer(AsyncWebsocketConsumer):
+class RoomConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.roomid = None
+
     async def connect(self):
+        self.roomid = self.scope['url_route']['kwargs']['roomid']
         await self.accept()
+        print(f'WebSocket 연결 성공! Room ID: {self.roomid}')
 
     async def disconnect(self, close_code):
         pass
 
     async def receive(self, text_data=None, bytes_data=None):
-        pass
+        res = json.loads(text_data)
+        event = res.get('event')
+        data = res.get('data')
 
     async def some_custom_method(self, event):
         pass
--- backend/routing.py
+++ backend/routing.py
@@ -1,18 +1,10 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from django.urls import re_path
-from channels.auth import AuthMiddlewareStack
-from myapp import consumers
+from myapp.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                [
-                    re_path(r"ws/some_endpoint/$", consumers.SomeConsumer.as_asgi()),
-                ]
-            )
-        ),
+        "websocket": URLRouter(websocket_urlpatterns),
     }
 )
