25c133f61c7c392e6f220d356867958dbbe06f78
raceStarter <baek2332@gmail.com>
2023-07-13 01:24:48 +0900

    Design: 로딩 페이지 요소 위치 조정 및 애니메이션 효과 추가

--- frontend/src/pages/LoadingPage.tsx
+++ frontend/src/pages/LoadingPage.tsx
@@ -1,41 +1,82 @@
 import cloud1 from "../assets/images/cloud1.svg";
 import cloud2 from "../assets/images/cloud2.svg";
 import cloud3 from "../assets/images/cloud3.svg";
-import cloud4 from "../assets/images/cloud4.svg";
 import sun from "../assets/images/sun.svg";
 
 function LoadingPage() {
   return (
     <div className="w-screen h-screen font-bmjua bg-[#E7F5FF] flex flex-col items-center justify-center text-3xl md:text-5xl xl:text-6xl 2xl:text-7xl">
-      <h1>태양이가 열심히 그림을 그리고 있어요~</h1>
+      <div>
+        <span className="relative animate-[animeTextup_1500ms_infinite_100ms]">
+          태양이가
+        </span>{" "}
+        <span className="relative animate-[animeTextup_1500ms_infinite_200ms]">
+          열심히
+        </span>{" "}
+        <span className="relative animate-[animeTextup_1500ms_infinite_300ms]">
+          그림을
+        </span>{" "}
+        <span className="relative animate-[animeTextup_1500ms_infinite_400ms]">
+          그리고
+        </span>{" "}
+        <span className="relative animate-[animeTextup_1500ms_infinite_500ms]">
+          있어요~
+        </span>
+      </div>
       <div className="flex">
         <img
           src={cloud1}
           alt="cloud"
-          className="relative z-10 w-[21vw] h-[31vh] top-[19vh] left-[8vw]"
+          className="relative z-10 w-[430px] h-[340px] top-[150px] left-[200px] animate-[updown_1s_infinite_ease-in-out_alternate]"
         />
         <img
           src={cloud2}
           alt="cloud"
-          className="relative z-20 w-[12vw] h-[12vh] top-[19vh] right-[2vw]"
+          className="relative z-20 w-[300px] h-[200px] top-[137px] right-[217px] animate-[updown_1500ms_infinite_ease-in-out_alternate]"
         />
         <img
           src={sun}
           alt="sun"
-          className="relative z-0 w-[24vw] h-[49vh] right-[6vw]"
+          className=" relative z-0 w-[550px] h-[450px] right-[180px] bottom-[30px] animate-[tilting_1500ms_infinite_ease-in-out_alternate]"
         />
         <img
           src={cloud3}
           alt="cloud"
-          className="relative w-[14vw] h-[23vh] right-[8vw] top-[10vh]"
+          className="relative w-[320px] h-[270px] right-[70px] bottom-[80px] animate-[updown_1s_infinite_ease-in-out_alternate]"
         />
         <img
-          src={cloud4}
+          src={cloud1}
           alt="cloud"
-          className="relative w-[9vw] h-[16vh] right-[22vw] top-[29vh]"
+          className="relative w-[430px] h-[330px] right-[580px] top-[110px] animate-[updown_1500ms_infinite_ease-in-out_alternate]"
         />
       </div>
-      <div className=" mt-[5vh]">PLEASE WAIT...</div>
+      {/* <div className=" mt-[5vh]">PLEASE WAIT...</div> */}
+      <div className=" mt-[5vh] flex">
+        <div>PLEASE</div>
+        <div className="pl-4">
+          <span className=" opacity-0 animate-[fade_1s_forwards_100ms_alternate]">
+            W
+          </span>
+          <span className=" opacity-0 animate-[fade_1s_forwards_200ms_alternate]">
+            A
+          </span>
+          <span className=" opacity-0 animate-[fade_1s_forwards_300ms_alternate]">
+            I
+          </span>
+          <span className=" opacity-0 animate-[fade_1s_forwards_400ms_alternate]">
+            T
+          </span>
+          <span className=" opacity-0 animate-[fade_1s_infinite_500ms_alternate]">
+            .
+          </span>
+          <span className=" opacity-0 animate-[fade_1s_infinite_700ms_alternate]">
+            .
+          </span>
+          <span className=" opacity-0 animate-[fade_1s_infinite_900ms_alternate]">
+            .
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,26 @@ export default {
       fontFamily: {
         bmjua: ["BMJUA", "sans-serif"],
       },
+      keyframes: {
+        animeTextup: {
+          "0%, 40%, 60%, 80%, 100%": { top: 0 },
+          "20%": { top: "-1rem" },
+        },
+        fade: { to: { opacity: 1 } },
+        updown: {
+          from: { "margin-top": 0 },
+          to: { "margin-top": "-1rem" },
+        },
+        tilting: {
+          from: { transform: "rotate(-15deg)" },
+        },
+      },
+      animation: {
+        animeTextup: "animeTextup 1500ms infinite",
+        fade: "fade 100ms forwards",
+        updown: "updown 1.4s infinite",
+        tilting: "tilting 1s infinite",
+      },
     },
   },
   plugins: [],
