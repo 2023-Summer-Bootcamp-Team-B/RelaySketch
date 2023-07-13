9e1c3e5b8ddf74379c05ef233f882963fc2d085b
yunjuwon <a77265192@gmail.com>
2023-07-13 21:06:18 +0900

    Feat: 게임시작 API 추가

--- backend/myapp/consumers.py
+++ backend/myapp/consumers.py
@@ -6,6 +6,8 @@ class RoomConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.roomid = None
+        # 게임 시작 기준, 현재 게임이 진행 됐는지 알 수 있다.
+        self.round = 0
 
     async def connect(self):
         self.roomid = self.scope['url_route']['kwargs']['roomid']
@@ -19,6 +21,24 @@ class RoomConsumer(AsyncWebsocketConsumer):
         res = json.loads(text_data)
         event = res.get('event')
         data = res.get('data')
+
+        #게임 시작 신호 처리
+        if event == "startGame":
+            # "게임시작" 출력
+            print(data)
+            # self.round 값을 1 변경
+            # 게임을 시작했을 알림
+            self.round = 1
+            # room에 있는 모든 인원에게 게임을 시작 신호 보냄
+            await self.channel_layer.group_send(
+                self.roomid,
+                {
+                    'event': 'gameStart',
+                    'data': {
+                        'round': round
+                    }
+                }
+            )
 
     async def some_custom_method(self, event):
         pass
