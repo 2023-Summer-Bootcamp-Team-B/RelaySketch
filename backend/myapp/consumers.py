fd2fcec4a1554b0013daee04cccf72c6c8ce5c84
seoin <leeseoin@gmail.com>
2023-07-18 02:10:45 +0900

    Fix: 웹소켓 연결 및 DB 테이블 생성 기능 누락 고침

--- backend/myapp/consumers.py
+++ backend/myapp/consumers.py
@@ -4,44 +4,145 @@ import time
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from .models import Room, SubRoom, Topic
+
 class RoomConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.room_id = None
+        self.room_group_name = None
+        self.sub_room_id = None
         self.last_activity_time = None
         self.ping_interval = 5  # 핑 메시지 전송 간격 (초)
         self.timeout = 10  # 연결 타임아웃 시간 (초)
         self.ping_task = None
-        self.additional_time = 10  # 추가 시간 (초)
-        self.subroom_id = None
-        self.topic_timer = None
+
     async def connect(self):
         self.room_id = self.scope['url_route']['kwargs']['roomid']
+        self.room_group_name = 'main_room_%s' % self.room_id
+
+        # 웹소켓 연결을 활성화하며 유저를 방에 참가시킴
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
         await self.accept()
-        print(f'WebSocket 연결 성공! Room ID: {self.room_id}')
+
         self.last_activity_time = time.time()
         self.ping_task = asyncio.ensure_future(self.send_ping())
+
+        # 서브 게임방 생성 로직
+        room = await sync_to_async(Room.objects.get)(id=self.room_id)
+        sub_room_count = await sync_to_async(SubRoom.objects.filter(room=room, delete_at=None).count)()
+
+        if sub_room_count >= 6:
+            error_message = '방이 가득 찼습니다.'
+            await self.send(text_data=json.dumps({'error': error_message}))
+            await self.close(1008)
+            return
+
+        sub_room = await sync_to_async(SubRoom.add_subroom)(room)  # 방 생성
+        self.sub_room_id = sub_room.id
+
+        await self.send(text_data=json.dumps({
+            'event': 'connected',
+            'data': {
+                'playerId': sub_room.id,
+            }
+        }))
+
+        # 서브룸을 전부 가져오는 로직을 구현한 뒤, 해당 데이터를 전송합니다.
+        sub_rooms = await sync_to_async(SubRoom.objects.filter)(room=room, delete_at=None)
+
+        players_data = await sync_to_async(
+            lambda: [
+                {
+                    'id': subroom.id,
+                    'name': subroom.first_player,
+                    'isHost': subroom.is_host,
+                }
+                for subroom in sub_rooms
+            ]
+        )()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'renew_list',
+                'message': {
+                    'event': 'renewList',
+                    'data': {
+                        'players': players_data,
+                    },
+                },
+            }
+        )
+
     async def disconnect(self, close_code):
         if self.ping_task:
             self.ping_task.cancel()
-        if self.topic_timer:
-            self.topic_timer.cancel()
-        # 연결 종료 시 처리할 로직 작성
+
+        # 서브 게임방 테이블을 삭제
+        sub_room = await sync_to_async(SubRoom.objects.get)(id=self.sub_room_id)
+        if sub_room:
+            await sync_to_async(sub_room.delete_subroom)()
+
+        # 서브 게임방이 비어있으면 메인 게임방 테이블을 삭제
+        room = await sync_to_async(Room.objects.get)(id=self.room_id)
+        remaining_subrooms_exists = await sync_to_async(SubRoom.objects.filter(room=room, delete_at=None).exists)()
+        if not remaining_subrooms_exists:
+            await sync_to_async(room.delete)()
+
+        # 서브룸을 전부 가져오는 로직을 구현한 뒤, 해당 데이터를 전송합니다.
+        sub_rooms = await sync_to_async(SubRoom.objects.filter)(room=room, delete_at=None)
+
+        players_data = await sync_to_async(
+            lambda: [
+                {
+                    'id': subroom.id,
+                    'name': subroom.first_player,
+                    'isHost': subroom.is_host,
+                }
+                for subroom in sub_rooms
+            ]
+        )()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'renew_list',
+                'message': {
+                    'event': 'renewList',
+                    'data': {
+                        'players': players_data,
+                    },
+                },
+            }
+        )
+
+        # 웹소켓 연결을 비활성화하며 유저를 방 그룹에서 제거
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
     async def receive(self, text_data=None, bytes_data=None):
         res = json.loads(text_data)
         event = res.get('event')
         data = res.get('data')
+
         if event == 'ping':
             print('ping received')
             await self.send(text_data=json.dumps({
                 'event': 'pong',
                 'data': 'pong'
             }))
+            self.last_activity_time = time.time()
         elif event == 'pong':
             print('pong received')
             self.last_activity_time = time.time()
         elif event == 'submitTopic':
             await self.handle_topic_submission(data)
+
     async def send_ping(self):
         while True:
             try:
@@ -55,82 +156,18 @@ class RoomConsumer(AsyncWebsocketConsumer):
             except Exception as e:
                 # 에러 처리 (예: 연결이 끊어짐)
                 break
+
             if self.last_activity_time is not None:
                 elapsed_time = time.time() - self.last_activity_time
                 if elapsed_time > self.timeout:
                     await self.close()
                     print('연결이 타임아웃되었습니다.')
                     break
+
     async def handle_topic_submission(self, data):
-        subroom_id = data.get("playerId")
-        topic_title = data.get("topicTitle")
-        if not subroom_id or not topic_title:
-            return
-        try:
-            # 해당하는 SubRoom 객체와 Topic 객체 생성
-            subroom = await sync_to_async(SubRoom.objects.get)(id=subroom_id)
-            topic = await sync_to_async(Topic.objects.create)(title=topic_title, sub_room=subroom)
-            # 주제 제출 알림을 방에 속한 모든 클라이언트에게 보냄
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    "type": "topic_submitted",
-                    "message": {
-                        "event": "topicSubmitted",
-                        "data": {
-                            "playerId": subroom.id,
-                            "topicTitle": topic.title,
-                        },
-                    },
-                },
-            )
-            # 주제를 제출한 후 타이머 초기화
-            if self.topic_timer:
-                self.topic_timer.cancel()
-            self.topic_timer = asyncio.ensure_future(self.force_submission(subroom))
-            self.subroom_id = subroom_id  # 주제 제출한 플레이어의 subroom_id 저장
-        except SubRoom.DoesNotExist:
-            pass
-    async def force_submission(self, subroom):
-        try:
-            # 추가 시간만큼 대기
-            await asyncio.sleep(self.additional_time)
-            # 주제를 입력하지 않았으면 연결 해제
-            topic_count = await sync_to_async(Topic.objects.filter)(sub_room=subroom).count()
-            if topic_count == 0:
-                await self.disconnect(1000)
-                # 추가 시간 만료 시, 다음 서브룸으로 연결 및 해당 서브룸 삭제
-                next_subroom = await sync_to_async(SubRoom.objects.filter)(
-                    room=subroom.room,
-                    is_host=False,
-                    delete_at=None
-                ).order_by('created_at').first()
-                if next_subroom:
-                    next_subroom.is_host = True
-                    await sync_to_async(next_subroom.save)()
-                await sync_to_async(subroom.delete_subroom)()
-                # 프론트엔드에 메시지 전송
-                await self.channel_layer.group_send(
-                    self.room_group_name,
-                    {
-                        "type": "add_time_discount",
-                        "message": {
-                            "event": "add_time_discount",
-                            "data": {
-                                "players": [
-                                    {
-                                        "id": subroom.id,
-                                        "name": subroom.first_player,
-                                        "isHost": subroom.is_host,
-                                    }
-                                ],
-                            },
-                        },
-                    },
-                )
-        except asyncio.CancelledError:
-            pass
-    async def add_time_discount(self, event):
-        # 추가 시간 정보를 프론트엔드에 전송
-        message_content = event["message"]
+        # 주제 제출 처리 로직 작성
+        pass
+
+    async def renew_list(self, event):
+        message_content = event['message']
         await self.send(text_data=json.dumps(message_content))
