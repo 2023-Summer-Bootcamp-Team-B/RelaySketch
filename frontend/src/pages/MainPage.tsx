0e31fda6ed709a8dabb889ed173b2bf475874948
Mayreeel <kuwon15@gmail.com>
2023-07-18 23:16:40 +0900

    design : 요소 배치 수정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,59 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useRef, useState } from "react";
-
+import sketch from "../assets/images/sketch_book_white.svg";
+import sun from "../assets/images/sun.svg";
 import cloud1 from "../assets/images/구름1.svg";
 import cloud2 from "../assets/images/구름2.svg";
-import sketch from "../assets/images/sketch_border.svg";
-import sun from "../assets/images/sun.svg";
 
 function MainPage() {
-  const [sketchWidth, setSketchWidth] = useState(0);
-  const [sketchHeight, setSketchHeight] = useState(0);
-  const sketchRef = useRef<HTMLImageElement>(null);
-  const childRef = useRef<HTMLDivElement>(null);
-
-  const childAspectRatio = 1;
-  const childDistanceFromC = 30; // Npx로 설정
-
-  useEffect(() => {
-    if (sketchRef.current && childRef.current) {
-      setSketchWidth(sketchRef.current.offsetWidth);
-      setSketchHeight(sketchRef.current.offsetHeight);
-    }
-  }, []);
-
-  useEffect(() => {
-    const resizeHandler = () => {
-      if (sketchRef.current && childRef.current) {
-        setSketchWidth(sketchRef.current.offsetWidth);
-        setSketchHeight(sketchRef.current.offsetHeight);
-      }
-    };
-    window.addEventListener("resize", resizeHandler);
-    return () => {
-      window.removeEventListener("resize", resizeHandler);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (childRef.current) {
-      const childHeight = (sketchHeight / 2) * childAspectRatio * 1.66;
-      const childWidth = (sketchWidth / 2) * childAspectRatio * 1.7;
-
-      childRef.current.style.height = `${childHeight}px`;
-      childRef.current.style.width = `${childWidth}px`;
-      childRef.current.style.transform = `translateY(${childDistanceFromC}px)`;
-    }
-  }, [sketchWidth]);
-
   return (
-    <div className="relative h-screen w-screen bg-[#E7F5FF]">
+    <div className="h-screen w-screen bg-[#E7F5FF] overflow-hidden">
       <div className="relative">
         <div className="absolute  w-[500px] left-[708px] top-[380px] animate-slider_left_invisible ">
           <img src={cloud2} alt="cloud 1" className="w-full h-full " />
         </div>
 
-        <div className="absolute  w-[500px] top-20- animate-slider_right">
+        <div className="absolute  w-[500px] top-20 animate-slider_right">
           <img src={cloud1} alt="cloud 1" className="w-full h-full" />
         </div>
 
@@ -61,40 +19,26 @@ function MainPage() {
           <img src={cloud1} alt="cloud 1" className="w-full h-full" />
         </div>
       </div>
-
-      <div className="relative flex items-center justify-center h-screen bottom-[20px]">
-        <img
-          src={sketch}
-          className="absolute z-10 w-[55%] "
-          ref={sketchRef}
-          alt="backgroud"
-        />
-        <div
-          className="absolute mb-1 bg-white"
-          style={{ width: sketchWidth, height: sketchHeight }}
-          ref={childRef}
-        >
-          <div>
-            <img
-              src={sun}
-              className="absolute z-20 w-1/3 h-auto pr-1 mt-3 animate-wiggle"
-              style={{ top: "0", left: "0" }}
-              alt="backgroud sun"
-            />
-          </div>
-
-          <div className="absolute inset-0 flex items-center justify-center pt-5 z-2 ">
+      <div className="relative flex items-center justify-center h-screen bottom-[30px]">
+        <img src={sketch} className="absolute" alt="background" />
+        <div className="relative">
+          <img
+            src={sun}
+            className="z-50 transform w-[210px] h-auto mr-[490px] mb-[210px] animate-wiggle"
+            alt="backgroundsun"
+          />
+          <div className="absolute inset-0 flex items-center justify-center pt-5 z-2 w-auto h-auto ">
             <p className="text-center transform -translate-y-[70px] font-crayon text-[110px]">
-              &nbsp;RELAY
+              RELAY
             </p>
             <p className="absolute text-center transform translate-y-[60px] font-crayon text-[110px]">
               SKETCH
             </p>
 
             <div className="absolute inset-0 flex items-center justify-center duration-75 transform translate-y-[180px] scale-1 z-2 animate-pulse">
-              <p className="text-[40px] font-crayon mb-3">
+              <span className="text-[40px] font-crayon mb-3 whitespace-nowrap">
                 PRESS ANY KEY TO PLAY
-              </p>
+              </span>
             </div>
           </div>
         </div>
--- frontend/src/pages/PlayerRoomPage.tsx
+++ frontend/src/pages/PlayerRoomPage.tsx
@@ -1,159 +1,128 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useRef, useState } from "react";
-
-import cloud1 from "../assets/images/구름1.svg";
-import cloud2 from "../assets/images/구름2.svg";
-import cloud3 from "../assets/images/구름3.svg";
+/* eslint-disable react/no-array-index-key */
 import invite from "../assets/images/invite_link.svg";
 import play from "../assets/images/play.svg";
 import sketch from "../assets/images/sketch_book_white.svg";
 import small_border from "../assets/images/small_box_border.svg";
+import cloud1 from "../assets/images/구름1.svg";
+import cloud2 from "../assets/images/구름2.svg";
+import cloud3 from "../assets/images/구름3.svg";
 
+type ArrType = { id: number; empty: boolean };
 
 function PlayerRoomPage() {
+  const arr1 = [
+    { id: 1, empty: false },
+    { id: 2, empty: false },
+    { id: 3, empty: false },
+    { id: 4, empty: false },
+    { id: 5, empty: false },
+    { id: 6, empty: true },
+  ];
+
   return (
-    <div className="h-screen w-screen bg-[#E7F5FF] flex items-center justify-center">
-      <img src={sketch} alt="Sketch" className="relative z-10 bottom-5 w-[55%]" />
-      <div className="absolute grid grid-cols-2 grid-rows-3 mt-16 gap-x-[300px] gap-y-[140px] mb-8 ">
-        <div className="relative flex items-center justify-center">
-          <div className="absolute  z-20 w-[300px] h-[100px]">
+    <div className="relative h-screen w-screen bg-[#E7F5FF] flex justify-center items-center overflow-hidden">
+      <img src={sketch} alt="sketch" className="absolute z-10 pb-[60px]" />
+      <div className="grid grid-cols-2 grid-rows-3 mb-8 w-[650px] h-[400px] mt-[10px] ">
+        {arr1.map((x: ArrType) =>
+          x.empty ? (
+            <div
+              className="relative z-20 w-[300px] h-[100px] items-center justify-center mt-8 ml-2"
+              key={x.id}
+            >
+              <img
+                src={small_border}
+                alt="small_border"
+                className="absolute z-30 w-full h-auto"
+              />
+              <p className="absolute z-30  ml-[90px] mt-[28px] font-crayon text-[40px]">
+                EMPTY
+              </p>
+              <div className="absolute bg-gray-400 z-39 h-[100%] w-[91%] top-2 left-3" />
+            </div>
+          ) : (
+            <div
+              className="relative z-20 w-[300px] h-[100px] items-center justify-center mt-8 ml-4"
+              key={x.id}
+            >
+              <img
+                src={small_border}
+                alt="small_border"
+                className="absolute z-30 w-full h-auto"
+              />
+              <p className="absolute z-30  ml-[70px] mt-[28px] font-crayon text-[40px]">
+                PLAYER{x.id}
+              </p>
+              <div className="absolute bg-[#E7F5FF] z-39 h-[100%] w-[91%] top-1 left-2.5" />
+            </div>
+          )
+        )}
+        <button className="relative" type="button">
+          <div className="absolute flex items-center justify-center ml-[100px] z-20 w-[190px] h-[65px] top-[90px]">
             <img
               src={small_border}
-              alt=""
-              className="absolute z-30 w-full h-auto"
+              alt="small_border"
+              className="absolute z-30"
             />
-            <p className="absolute z-30  ml-[70px] mt-[33px] font-crayon text-[40px]">
-              PLAYER1
-            </p>
-            <div className="absolute bg-[#E7F5FF] z-39 h-[100%] w-[91%] top-1 left-2.5" />
-          </div>
-        </div>
+            <img
+              src={invite}
+              alt="invite"
+              className="absolute z-30 mt-2 ml-5 left-0"
+            />
 
-        <div className="relative flex items-center justify-center">
-          <div className="absolute  z-20 w-[300px] h-[100px]">
+            <span className="absolute z-30 ml-[70px] font-crayon text-[40px]">
+              Link
+            </span>
+            <div className="absolute bg-white z-39 h-[100%] w-[91%] top-1 left-2.5" />
+          </div>
+        </button>
+
+        <button className="relative" type="button">
+          <div className="absolute flex items-center justify-center ml-10 z-20 w-[190px] h-[65px] top-[90px]">
             <img
               src={small_border}
-              alt=""
+              alt="small_border"
               className="absolute z-30 w-full h-auto"
             />
-            <p className="absolute z-30  ml-[70px] mt-[33px] font-crayon text-[40px]">
-              PLAYER1
-            </p>
-            <div className="absolute bg-[#E7F5FF] z-39 h-[100%] w-[91%] top-1 left-2.5" />
-          </div>
-        </div>
+            <img src={play} alt="" className="absolute z-30 mt-2 ml-5 left-0" />
 
-        <div className="relative flex items-center justify-center">
-          <div className="absolute  z-20 w-[300px] h-[100px]">
-            <img
-              src={small_border}
-              alt=""
-              className="absolute z-30 w-full h-auto"
-            />
-            <p className="absolute z-30  ml-[70px] mt-[33px] font-crayon text-[40px]">
-              PLAYER1
+            <p className="absolute z-30  ml-[70px] font-crayon text-[40px]">
+              Start
             </p>
-            <div className="absolute bg-[#E7F5FF] z-39 h-[100%] w-[91%] top-1 left-2.5" />
+            <div className="absolute bg-white z-39 h-[100%] w-[91%] top-1 left-2.5" />
           </div>
-        </div>
+        </button>
 
-        <div className="relative flex items-center justify-center">
-          <div className="absolute  z-20 w-[300px] h-[100px]">
-            <img
-              src={small_border}
-              alt=""
-              className="absolute z-30 w-full h-auto"
-            />
-            <p className="absolute z-30  ml-[70px] mt-[33px] font-crayon text-[40px]">
-              PLAYER1
-            </p>
-            <div className="absolute bg-[#E7F5FF] z-39 h-[100%] w-[91%] top-1 left-2.5" />
-          </div>
-        </div>
-
-        <div className="relative flex items-center justify-center">
-          <div className="absolute  z-20 w-[300px] h-[100px]">
-            <img
-              src={small_border}
-              alt=""
-              className="absolute z-30 w-full h-auto"
-            />
-            <p className="absolute z-30  ml-[70px] mt-[33px] font-crayon text-[40px]">
-              PLAYER1
-            </p>
-            <div className="absolute bg-[#E7F5FF] z-39 h-[100%] w-[91%] top-1 left-2.5" />
-          </div>
-        </div>
-
-        <div className="relative flex items-center justify-center">
-          <div className="absolute  z-20 w-[300px] h-[100px]">
-            <img
-              src={small_border}
-              alt=""
-              className="absolute z-30 w-full h-auto"
-            />
-            <p className="absolute z-30  ml-[90px] mt-[33px] font-crayon text-[40px]">
-              EMPTY
-            </p>
-            <div className="absolute bg-gray-400 z-39 h-[100%] w-[91%] top-2 left-3" />
-          </div>
-        </div>
-      </div>
-
-      <div className="absolute flex items-center justify-center left-[500px] mt-[650px] ml-10">
-        <div className="absolute  z-20 w-[190px] h-[65px]">
+        <div>
           <img
-            src={small_border}
-            alt=""
-            className="absolute z-30 w-full h-auto"
+            src={cloud1}
+            alt="cloud1"
+            className="absolute z-0 bottom-96 w-96 right-[200px]"
           />
-          <img src={invite} alt="" className="absolute z-30 mt-5 ml-5" />
-
-          <p className="absolute z-30  ml-[70px] mt-[10px] font-crayon text-[40px]">
-            Link
-          </p>
-          <div className="absolute bg-white z-39 h-[100%] w-[91%] top-1 left-2.5" />
         </div>
-      </div>
 
-      <div className="absolute flex items-center justify-center right-[500px] mt-[650px] mr-10">
-        <div className="absolute  z-20 w-[190px] h-[65px]">
+        <div>
           <img
-            src={small_border}
-            alt=""
-            className="absolute z-30 w-full h-auto"
+            src={cloud3}
+            alt="cloud3"
+            className="absolute z-0 top-10 right-[200px]"
           />
-          <img src={play} alt="" className="absolute z-30 mt-5 ml-5" />
-
-          <p className="absolute z-30  ml-[70px] mt-[10px] font-crayon text-[40px]">
-            Start
-          </p>
-          <div className="absolute bg-white z-39 h-[100%] w-[91%] top-1 left-2.5" />
         </div>
-      </div>
 
-      <div>
-        <img
-          src={cloud1}
-          alt=""
-          className="absolute z-0 bottom-96 w-96 right-[200px]"
-        />
-      </div>
+        <div>
+          <img
+            src={cloud1}
+            alt="cloud1"
+            className="absolute z-0 left-[200px] bottom"
+          />
+        </div>
 
-      <div>
-        <img
-          src={cloud3}
-          alt=""
-          className="absolute z-0 top-10 right-[200px]"
-        />
-      </div>
-
-      <div>
-        <img src={cloud1} alt="" className="absolute z-0 left-[200px] bottom" />
-      </div>
-
-      <div>
-        <img src={cloud2} alt="" className="absolute z-0 left-[200px]" />
+        <div>
+          <img
+            src={cloud2}
+            alt="cloud2"
+            className="absolute z-0 left-[200px]"
+          />
+        </div>
       </div>
     </div>
   );
