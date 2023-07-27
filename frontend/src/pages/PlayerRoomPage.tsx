f3eaee92a5be00289b54f7d322996bc1ec420a88
김진서 <breadd123456@gimjinseoui-MacBookPro.local>
2023-07-27 16:55:55 +0900

    프리티어 관련 에러 추가수정

--- frontend/src/pages/PlayerRoomPage.tsx
+++ frontend/src/pages/PlayerRoomPage.tsx
@@ -132,7 +132,7 @@ const PlayerRoomPage = observer(() => {
 
       if (
         roomstate.findIndex(
-          (item) => !(item.playerId === myId) && item.playerName === inputValue
+          (item) => !(item.playerId === myId) && item.playerName === inputValue,
         ) !== -1
       ) {
         SetValueState("This text is too Many");
@@ -222,7 +222,7 @@ const PlayerRoomPage = observer(() => {
                 )}
               </div>
             </div>
-          )
+          ),
         )}
         <button
           className="relative"
