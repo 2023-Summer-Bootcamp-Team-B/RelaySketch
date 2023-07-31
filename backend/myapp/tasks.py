272157e4715db29ff66d5316d8e43de8dd273fa0
Mayreeel <kuwon15@gmail.com>
2023-08-01 06:56:10 +0900

    fix : chain test

--- backend/myapp/tasks.py
+++ backend/myapp/tasks.py
@@ -1,9 +1,12 @@
 import os
-from celery import shared_task
+import uuid
+from celery import shared_task, chain
 import openai
 import requests
 from openai import InvalidRequestError
-from celery import chain
+from storages.backends.s3boto3 import S3Boto3Storage
+from datetime import datetime
+from myapp.models import Room, SubRoom, Topic
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 client_id = os.getenv("NAVER_CLIENT_ID")
@@ -35,14 +38,13 @@ def translate_text(text):
         translated_text = response_data["message"]["result"]["translatedText"]
         return translated_text
     else:
-        # 처리 실패 시 예외 처리 등을 수행할 수 있습니다.
         raise Exception("Translation failed")
 
 
-@shared_task(bind=True, max_retries=3)
-def create_image(self, title):
+@shared_task
+def create_image(translated_text):
     try:
-        response = openai.Image.create(prompt=title, n=1, size="256x256")
+        response = openai.Image.create(prompt=translated_text, n=1, size="256x256")
         image_url = response["data"][0]["url"]
         return image_url
     except InvalidRequestError as e:
@@ -50,21 +52,38 @@ def create_image(self, title):
         print(error_message)
         return {'error': error_message}
     except Exception as e:
-        # Handle other exceptions
-        print(f"An unexpected error occurred: {e}")
-        return {'error': str(e)}
-
-
-@shared_task(bind=True)
-def upload_image_to_s3(self, image_url):
-    try:
-        s3_image_url = upload_image_to_s3(image_url, "image")
-        return f"https://{AWS_STORAGE_BUCKET_NAME}.s3-{AWS_S3_REGION_NAME}.amazonaws.com/{s3_image_url}"
-    except Exception as e:
         print(f"An unexpected error occurred: {e}")
         return {'error': str(e)}
 
 
 @shared_task
+def upload_image_to_s3(image_url, filename):
+    from io import BytesIO
+    import requests
+
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        image_data = BytesIO(response.content)
+        storage = S3Boto3Storage()
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        random_string = str(uuid.uuid4().hex[:6])
+        unique_filename = f"images/{filename}_{now}_{random_string}.png"
+        s3_image_url = storage.save(unique_filename, image_data)
+        return s3_image_url
+    else:
+        raise Exception("Failed to upload image to S3")
+
+
+@shared_task
 def chain_process(text):
     chain(translate_text.s(text), create_image.s(), upload_image_to_s3.s()).apply_async()
+
+
+@shared_task
+def clear_data():
+    for room in Room.objects.all():
+        room.hard_delete()
+    for sub_room in SubRoom.objects.all():
+        sub_room.hard_delete()
+    for topic in Topic.objects.all():
+        topic.hard_delete()
